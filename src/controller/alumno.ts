ca9d68a687ceb8b9be114cc64e29761f4b6a2c15
ManuelPerezF <A01741957@tec.mx>
2025-04-06 19:38:17 -0600

    Implementar mensaje dependiendo de la calificacion y el adeudo.

--- src/controller/alumno.ts
+++ src/controller/alumno.ts
@@ -5,9 +5,27 @@ const alumnoService = new AlumnoService();
 class AlumnoController {
     async getAllAlumnos() {
         const alumnos = await alumnoService.getAllAlumnos();
-        return alumnos
-    }
 
+        const resultado = alumnos.map(alumno => {
+            var mensaje = '';
+
+            if (alumno.calificacion > 90 && !alumno.adeudos) {
+                mensaje = 'Te graduaste con honores';
+            } else if (alumno.calificacion > 90 && alumno.adeudos) {
+                mensaje = 'Tenias honores pero tienes un adeudo';
+            } else if (alumno.calificacion >= 70 && alumno.calificacion <= 90) {
+                mensaje = 'Pasaste muy apenas';
+            } else if (alumno.calificacion < 70 && !alumno.adeudos) {
+                mensaje = 'Como no tienes adeudo tienes derecho a examen de recuperacion';
+            } else if (alumno.calificacion < 70 && alumno.adeudos) {
+                mensaje = 'Expulsado';
+            }
+
+            return {matricula: alumno.matricula, mensaje};
+        });
+
+        return resultado;
+    }
 }
 
 export default AlumnoController;
